f19bac691b3f7c3ba5c7ea1046b632a6e1abe1bb
gvasu943 <giridhar_vasu@comcast.com>
2026-01-20 19:43:03 +0530

    20th Jan Changes.

---
+++ main.js
@@ -0,0 +1,5 @@
+// Import specific tools
+import { add, pi } from './mathTools.js';
+
+console.log("Pi is roughly " + pi);
+console.log("2 + 3 = " + add(2, 3));
---
+++ modulesES6ImportExport.js
@@ -0,0 +1,9 @@
+// Topic of the Day: Modules (ES6 Import/Export)
+// Explanation: Just like Python has import, modern JavaScript allows you to split code into files.
+// export: Makes a function/variable available to other files.
+// import: Brings it in.
+// Note: This requires a server or specific HTML setup (type="module").
+
+// File: mathTools.js
+
+// File: main.js
